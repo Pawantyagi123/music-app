bf69220621f24570eb5b2aabab5b7370fe1b379d
pawantyagi123 <pawantyaginnl2002@gmail.com>
2024-08-22 14:47:27 +0530

    update

--- Task3/src/Components/SongList.jsx
+++ Task3/src/Components/SongList.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export default function SongList({  songs, handleSongSelect  }) {
+
+  const titleLength = (title) =>{
+    if (!title) return ""; // Return an empty string if title is undefined or null
+    return title.length > 10 ? `${title.substring(0, 10)}...` : title;
+  }
   return (
     <div className="song-list">
       <h1 style={{padding: "20px"}}>Song List</h1>
@@ -8,7 +13,7 @@ export default function SongList({  songs, handleSongSelect  }) {
       {songs.map(song => (
         <li key={song.id} onClick={() => handleSongSelect(song)}>
           <img src={song.image} alt={song.title} width="50" className='listimg'/>
-          <h3>{song.title}</h3> 
+          <h3>{titleLength(song.title)}</h3> 
           
         </li>
       ))}
